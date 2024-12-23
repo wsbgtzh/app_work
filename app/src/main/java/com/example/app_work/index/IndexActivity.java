837eb1c7f306c1ce3bdb056efc1f91a8fc80f74b
Zuhan Tang <tangzuhan@gmail.com>
2024-12-23 22:37:01 +0800

    create index

---
+++ app/src/main/java/com/example/app_work/index/IndexActivity.java
@@ -0,0 +1,16 @@
+package com.example.app_work.index;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.app_work.R;
+
+public class IndexActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
